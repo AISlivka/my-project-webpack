24883422d8440e1f03217812cd68d40a4af89705
Андрей Сливка <ai.slivka@stc.team>
2024-01-28 22:42:11 +0300

    add image && add json

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -1,26 +1,54 @@
 <template>
   <CHeader></CHeader>
   <RouterLink to="/about"> СТРАНИЦА ABOUT </RouterLink>
-  <div class="image__list">
-    <div class="image">
-      <img :src="imagePng" alt="png" />
-    </div>
-    <div class="image">
-      <img :src="imageJpeg" alt="jpeg" />
-    </div>
-    <div class="image">
-      <iconSvg></iconSvg>
-    </div>
-  </div>
 
   <div class="home-page">HOME PAGE COMPONENT</div>
+  <div class="home-page__buttons" style="padding: 24px;">
+    <input type="file" @input="onImageUploaded">
+    <button @click="submitJson">application/json</button>
+    <button @click="submitForm">application/x-www-form-urlencoded</button>
+  </div>
 </template>
 
 <script setup>
 import CHeader from "@/components/CHeader/CHeader.vue"
-import imagePng from "@/assets/images/image.png"
-import imageJpeg from "@/assets/images/image.jpeg"
-import iconSvg from "@/assets/images/icon.svg?component"
+import jsonPackage from '/package.json'
+import { ref } from 'vue';
+
+const formData = ref(jsonPackage);
+
+const submitJson = async () => {
+  try {
+    const response = await fetch('http://localhost:3001/form-json', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData.value)
+    });
+    console.log('json отправлен:', jsonPackage.name);
+  } catch (error) {
+    console.error('Ошибка json:', error);
+  }
+}
+
+const onImageUploaded = async (event) => {
+  try {
+    const img = event.target.files[0]
+    const formData = new FormData()
+        
+    formData .append('image', img)
+
+    const response = await fetch('http://localhost:3001/upload', {
+      method: 'POST',
+      body: formData,
+    });
+    console.log('Файл отправлен:', img.name);
+  } catch (error) {
+    console.error('Ошибка image:', error);
+  }
+}
+
 </script>
 
 <style>
