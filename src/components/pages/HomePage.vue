f8b7b2e31ecdd2d9f9ca4e657eaa61ee79850210
Андрей Сливка <ai.slivka@stc.team>
2024-01-31 20:53:55 +0300

    add useFetch buttons

--- src/components/pages/HomePage.vue
+++ src/components/pages/HomePage.vue
@@ -5,67 +5,132 @@
   <div class="home-page">
     <h2>Fetch API</h2>
     <div class="home-page__buttons">
-      <input type="file" @input="onImageUploadedFetchApi">
+      <input type="file" @input="onImageUploadedFetchApi" />
       <button @click="submitJsonFetchApi">submitJsonFetchApi</button>
       <button @click="submitFormFetchApi">submitFormFetchApi</button>
     </div>
     <h2>Axios</h2>
     <div class="home-page__buttons">
-      <input type="file" @input="onImageUploadedAxios">
+      <input type="file" @input="onImageUploadedAxios" />
       <button @click="submitJsonAxios">submitJsonAxios</button>
       <button @click="submitFormAxios">submitFormAxios</button>
     </div>
-    <!-- <h2>useFetch</h2>
+    <h2>useFetch</h2>
     <div class="home-page__buttons">
-      <input type="file" @input="onImageUploadedUseFetch">
+      <input type="file" @input="onImageUploadedUseFetch" />
       <button @click="submitJsonUseFetch">submitJsonUseFetch</button>
       <button @click="submitFormUseFetch">submitFormUseFetch</button>
-    </div> -->
+    </div>
   </div>
-  
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue"
 import CHeader from "@/components/CHeader/CHeader.vue"
-import jsonPackage from '/package.json'
-import axios from 'axios';
-// import { useFetch } from '@vueuse/core';
+import jsonPackage from "/package.json"
+import axios from "axios"
+import { useFetch } from "@vueuse/core"
 
-const formData = ref(jsonPackage);
+const formData = ref(jsonPackage)
+
+// useFetch
+const submitJsonUseFetch = async () => {
+  const { statusCode } = await useFetch(
+    "http://localhost:3001/form-content",
+    {
+      body: new URLSearchParams({
+        name: "Andrey",
+        surname: "Slivka",
+      }),
+    },
+    {
+      beforeFetch({ options }) {
+        options.headers = {
+          ...options.headers,
+          ["Content-Type"]: "application/x-www-form-urlencoded",
+        }
+        return { options }
+      },
+    },
+  ).post()
+  if (statusCode.value === 200) {
+    console.log("Форма useFetch отправлена")
+  } else {
+    console.error("Ошибка useFetch формы:", error)
+  }
+}
+
+const submitFormUseFetch = async () => {
+  const { statusCode } = await useFetch(
+    "http://localhost:3001/form-json",
+    {
+      body: JSON.stringify(formData.value),
+    },
+    {
+      beforeFetch({ options }) {
+        options.headers = {
+          ...options.headers,
+          ["Content-Type"]: "application/json",
+        }
+        return { options }
+      },
+    },
+  ).post()
+  if (statusCode.value === 200) {
+    console.log("json useFetch отправлена")
+  } else {
+    console.error("Ошибка useFetch json:", error)
+  }
+}
+
+const onImageUploadedUseFetch = async (event) => {
+  const img = event.target.files[0]
+  const formData = new FormData()
+
+  formData.append("image", img)
+
+  const { statusCode } = await useFetch("http://localhost:3001/upload", {
+    body: formData,
+  }).post()
+  if (statusCode.value === 200) {
+    console.log("Image useFetch отправлена")
+  } else {
+    console.error("Ошибка useFetch image:", error)
+  }
+}
 
 // Fetch API
 const submitFormFetchApi = async () => {
-  const formData = new URLSearchParams();
-  formData.append('name', 'Andrey');
-  formData.append('surname', 'Slivka');
+  const formData = new URLSearchParams()
+  formData.append("name", "Andrey")
+  formData.append("surname", "Slivka")
 
   try {
-    const response = await fetch('http://localhost:3001/form-content', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3001/form-content", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: formData
-    });
-    console.log('Форма fetch API отправлена');
+      body: formData,
+    })
+    console.log("Форма fetch API отправлена")
   } catch (error) {
-    console.error('Ошибка fetch API формы:', error);
+    console.error("Ошибка fetch API формы:", error)
   }
-};
+}
 
 const submitJsonFetchApi = async () => {
   try {
-    const response = await fetch('http://localhost:3001/form-json', {
-      method: 'POST',
+    const response = await fetch("http://localhost:3001/form-json", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify(formData.value)
-    });
-    console.log('json fetch API отправлен:', jsonPackage.name);
+      body: JSON.stringify(formData.value),
+    })
+    console.log("json fetch API отправлен:", jsonPackage.name)
   } catch (error) {
-    console.error('Ошибка fetch API json:', error);
+    console.error("Ошибка fetch API json:", error)
   }
 }
 
@@ -73,67 +138,71 @@ const onImageUploadedFetchApi = async (event) => {
   try {
     const img = event.target.files[0]
     const formData = new FormData()
-        
-    formData.append('image', img)
 
-    const response = await fetch('http://localhost:3001/upload', {
-      method: 'POST',
+    formData.append("image", img)
+
+    const response = await fetch("http://localhost:3001/upload", {
+      method: "POST",
       body: formData,
-    });
-    console.log('Файл fetch API отправлен:', img.name);
+    })
+    console.log("Файл fetch API отправлен:", img.name)
   } catch (error) {
-    console.error('Ошибка fetch API image:', error);
+    console.error("Ошибка fetch API image:", error)
   }
 }
 
 // axios
 
 const submitFormAxios = async () => {
-  const formData = new URLSearchParams();
-  formData.append('name', 'Andrey');
-  formData.append('surname', 'Slivka');
+  const formData = new URLSearchParams()
+  formData.append("name", "Andrey")
+  formData.append("surname", "Slivka")
 
   try {
-    const response = await axios.post('http://localhost:3001/form-content', formData, {
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    });
-    console.log('Форма Axios отправлена');
+    const response = await axios.post(
+      "http://localhost:3001/form-content",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+    )
+    console.log("Форма Axios отправлена")
   } catch (error) {
-    console.error('Ошибка Axios формы:', error);
+    console.error("Ошибка Axios формы:", error)
   }
-};
+}
 
 const submitJsonAxios = async () => {
   try {
-    const response = await axios.post('http://localhost:3001/form-json', formData, {
-      headers: {
-        'Content-Type': 'application/json'
+    const response = await axios.post(
+      "http://localhost:3001/form-json",
+      formData,
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
       },
-    });
-    console.log('json Axios отправлен:', jsonPackage.name);
+    )
+    console.log("json Axios отправлен:", jsonPackage.name)
   } catch (error) {
-    console.error('Ошибка Axios json:', error);
+    console.error("Ошибка Axios json:", error)
   }
 }
 
 const onImageUploadedAxios = async (event) => {
   try {
-    const img = event.target.files[0];
-    const formData = new FormData();
-    formData.append('image', img);
+    const img = event.target.files[0]
+    const formData = new FormData()
+    formData.append("image", img)
 
-    const response = await axios.post('http://localhost:3001/upload', formData);
-    console.log('Файл Axios отправлен:', img.name);
+    const response = await axios.post("http://localhost:3001/upload", formData)
+    console.log("Файл Axios отправлен:", img.name)
   } catch (error) {
-    console.error('Ошибка Axios image:', error);
+    console.error("Ошибка Axios image:", error)
   }
 }
-
-// useFetch
-
-
 </script>
 
 <style>
@@ -154,7 +223,7 @@ const onImageUploadedAxios = async (event) => {
 
 .home-page {
   padding: 24px;
-  margin: 24px; 
+  margin: 24px;
   box-shadow: 0 2px 8px 0 rgb(0 0 0 / 50%);
   border-radius: 16px;
   max-width: 800px;
